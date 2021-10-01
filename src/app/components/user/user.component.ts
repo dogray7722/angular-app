ee9d6399c63df8fb7d04710ef0cac684e58355a6
Dave Gray <dave@choozle.com>
2021-10-01 09:18:44 -0600

    initial

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css'],
+  styles: [`
+    h2 {
+      color: blue
+    }
+  `]
+})
+
+export class UserComponent {
+  // Properties
+  firstName = 'John';
+  lastName = 'Doe';
+  age = 30;
+
+  // Methods
+  constructor() {
+    // this.sayHello();
+    console.log(`Yesterday you were ${this.age}.`)
+      this.hasBirthday()
+    console.log(`But today's your birthday, so now you are ${this.age} years old.`) 
+    console.log("Happy Birthday old fuck!")
+  }
+
+  sayHello() {
+    console.log(`Hello ${this.firstName}`)
+  }
+
+  hasBirthday() {
+    this.age += 1;
+  }
+
+
+}
