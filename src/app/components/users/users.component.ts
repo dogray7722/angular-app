731205db503c023f838c105ec08bb6b170edb1da
dogray7722 <david.gray61@du.edu>
2021-10-04 13:03:33 -0600

    ngIf and templates

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -9,51 +9,93 @@ import { User } from '../../models/User'
 })
 export class UsersComponent implements OnInit {
   users: User[];
+  showExtended: boolean = true;
+  loaded: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.users = [
+          {
+            firstName: "John",
+            lastName: "Doe",
+            age: 30,
+            address: {
+              street: "50 Main St",
+              city: "Denver",
+              state: "CO",
+            },
+          },
+          {
+            firstName: "Janet",
+            lastName: "Bilco",
+            age: 34,
+            address: {
+              street: "501 1st St",
+              city: "Cincinnati",
+              state: "OH",
+            },
+          },
+          {
+            firstName: "Michael",
+            lastName: "Hunter",
+            age: 46,
+            address: {
+              street: "105 Fuck St",
+              city: "Arvada",
+              state: "CO",
+            },
+          }
+      ];
+
+      this.loaded = true;
+    }, 2000);
+
+    this.showExtended = false;
+
     this.users = [
-      {
-        firstName: "John",
-        lastName: "Doe",
-        age: 30,
-        address: {
-          street: "50 Main St",
-          city: "Denver",
-          state: "CO",
-        },
-      },
-      {
-        firstName: "Janet",
-        lastName: "Bilco",
-        age: 34,
-        address: {
-          street: "501 1st St",
-          city: "Cincinnati",
-          state: "OH",
-        },
-      },
-      {
-        firstName: "Michael",
-        lastName: "Hunter",
-        age: 46,
-        address: {
-          street: "105 Fuck St",
-          city: "Arvada",
-          state: "CO",
-        },
-      }
+    //   {
+    //     firstName: "John",
+    //     lastName: "Doe",
+    //     age: 30,
+    //     address: {
+    //       street: "50 Main St",
+    //       city: "Denver",
+    //       state: "CO",
+    //     },
+    //   },
+    //   {
+    //     firstName: "Janet",
+    //     lastName: "Bilco",
+    //     age: 34,
+    //     address: {
+    //       street: "501 1st St",
+    //       city: "Cincinnati",
+    //       state: "OH",
+    //     },
+    //   },
+    //   {
+    //     firstName: "Michael",
+    //     lastName: "Hunter",
+    //     age: 46,
+    //     address: {
+    //       street: "105 Fuck St",
+    //       city: "Arvada",
+    //       state: "CO",
+    //     },
+    //   }
     ];
-    this.addUser({
-      firstName: "David",
-      lastName: "CornSlurp",
-      age: 19,
-      address: {
-        street: "15 NutHugger Ave",
-        city: "Stillwater",
-        state: "OK",
-      }
-    });
+    // this.addUser({
+    //   firstName: "David",
+    //   lastName: "CornSlurp",
+    //   age: 19,
+    //   address: {
+    //     street: "15 NutHugger Ave",
+    //     city: "Stillwater",
+    //     state: "OK",
+    //   }
+    // });
   }
 
   addUser(user: User) {
