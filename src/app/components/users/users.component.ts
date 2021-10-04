ce33f796f53e5ef2060db872232dc5a4bbff735e
dogray7722 <david.gray61@du.edu>
2021-10-04 10:45:48 -0600

    update tsconfig.json to set strict mode false so properties do not have to be initialized in the constructor and can be done within onInit instead

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../../models/User'
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
-
+  users: User[];
   constructor() { }
 
   ngOnInit(): void {
-    console.log('init...')
+    this.users = [];
   }
 
 }
