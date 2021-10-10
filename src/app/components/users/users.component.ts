cfacd7396efaa6c548baab51343f647c5c3aef3e
dogray7722 <david.gray61@du.edu>
2021-10-10 17:16:52 -0600

    experiment with property binding

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -11,50 +11,54 @@ export class UsersComponent implements OnInit {
   users: User[];
   showExtended: boolean = true;
   loaded: boolean = false;
+  enableAdd: boolean = true;
 
   constructor() { }
 
   ngOnInit(): void {
   
       this.users = [
-          {
-            firstName: "John",
-            lastName: "Doe",
-            age: 30,
-            address: {
-              street: "50 Main St",
-              city: "Denver",
-              state: "CO",
-            },
+        {
+          firstName: "John",
+          lastName: "Doe",
+          age: 30,
+          address: {
+            street: "50 Main St",
+            city: "Denver",
+            state: "CO",
           },
-          {
-            firstName: "Janet",
-            lastName: "Bilco",
-            age: 34,
-            address: {
-              street: "501 1st St",
-              city: "Cincinnati",
-              state: "OH",
-            },
+          image: "http://lorempixel.com/300/300/people/3",
+        },
+        {
+          firstName: "Janet",
+          lastName: "Bilco",
+          age: 34,
+          address: {
+            street: "501 1st St",
+            city: "Cincinnati",
+            state: "OH",
           },
-          {
-            firstName: "Michael",
-            lastName: "Hunter",
-            age: 46,
-            address: {
-              street: "105 Fuck St",
-              city: "Arvada",
-              state: "CO",
-            },
-          }
+          image: "http://lorempixel.com/300/300/people/5",
+        },
+        {
+          firstName: "Michael",
+          lastName: "Hunter",
+          age: 46,
+          address: {
+            street: "105 Fuck St",
+            city: "Arvada",
+            state: "CO",
+          },
+          image: "http://lorempixel.com/300/300/people/8",
+        },
       ];
 
     this.loaded = true;
 
-    this.addUser({
-      firstName: "David",
-      lastName: "CornSlurp"
-    });
+    // this.addUser({
+    //   firstName: "David",
+    //   lastName: "CornSlurp"
+    // });
   }
 
   addUser(user: User) {
